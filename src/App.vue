c34f70b41ad094807c3f3cde295526daf630fc5e
MariaYminova <vabylia96@gmail.com>
2024-03-22 13:40:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <MainPage />
+  </div>
+</template>
+
+<script>
+import MainPage from "./components/MainPage.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    MainPage,
+  },
+};
+</script>
+
+<style lang="scss">
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card">
+    <div class="card__heading">{{ title }}</div>
+    <div class="card__text">{{ body }}</div>
+    <div class="card__author">{{ username }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Card",
+
+  props: {
+    title: String,
+    body: String,
+    username: String,
+  },
+};
+</script>
+
+<style lang="scss">
+.card {
+  padding: 15px;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  background-color: #fff;
+  box-shadow: 0px 1px 2px #0000005c;
+
+  &__heading {
+    color: #6596e0;
+    margin-bottom: 10px;
+  }
+
+  &__text {
+    color: #454647;
+    margin-bottom: 15px;
+  }
+
+  &__author {
+    color: #999a9b;
+  }
+}
+</style>
